1075d68991a12c97b12a8e98405c0925fc9f6d2e
duvanpalomino <duvanpalomino@gmail.com>
2023-10-02 14:53:35 -0500

    Actualizacion children’ is missing in props validation

--- src/container/Layout.jsx
+++ src/container/Layout.jsx
@@ -1,11 +1,19 @@
 // import React from 'react';
+import PropTypes from 'prop-types';
 
 const Layout = ({ children }) => {
+
+    Layout.propTypes = {
+        children: PropTypes.node.isRequired,
+    }
+
     return (
         <div className='flex flex-col items-center mt-20'>
             { children }
         </div>
     );
 }
+
+
 
 export default Layout;
