f9034c9b75828939d5ee25ee929aa3da1bb22b55
duvanpalomino <duvanpalomino@gmail.com>
2023-10-04 15:12:10 -0500

    Conectando con la API Fake Store

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,26 @@
 // import React from 'react';
+import { useState, useEffect } from 'react';
+import Card from "../container/Card";
 
 const Home = () => {
+
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(response => response.json())
+            .then(data => setItems(data))
+    }, []); 
+
     return (
         <>
-            Home
+            <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+                {
+                items?.map(item => (
+                    <Card key={item.id} data={item}/>
+                ))
+                }
+            </div>
         </>
     );
 }
