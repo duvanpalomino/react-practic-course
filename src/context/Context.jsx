6246bbbe1edd27d64f4b7c8a51d140c4563605af
duvanpalomino <duvanpalomino@gmail.com>
2023-10-06 09:38:47 -0500

    Maquetando el Product Detail

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -11,11 +11,20 @@ export const ShoppingCartProvider = ({ children }) => {
     };
 
     const [count, setCount] = useState(0);
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+
+    const openProductDetail = () => setIsProductDetailOpen(true);
+    
+    const closeProductDetail = () => setIsProductDetailOpen(false);
+
 
     return (
         <ShoppingCartContext.Provider value={{
             count,
             setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
         }}>
             { children }
         </ShoppingCartContext.Provider>
